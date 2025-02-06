08db0030f663c62117b0663db93562d8760ce563
Jagger Gourley <jaggergourley@gmail.com>
2025-02-06 15:56:38 -0600

    gettin' her started

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.querySelector("game");
+function draw() {
+  const canvas = document.getElementById("myCanvas");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+  }
+}
+window.addEventListener("load", draw);
