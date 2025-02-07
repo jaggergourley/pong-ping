de941eab28dd7baf9cce215c0aac47bf51b2bf98
Jagger Gourley <jaggergourley@gmail.com>
2025-02-07 11:34:54 -0600

    Drew paddles and pong, wow!

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function draw() {
   const canvas = document.getElementById("myCanvas");
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.fillRect(400, 400, 10, 10);
+    ctx.fillRect(10, 400, 10, 50);
+    ctx.fillRect(780, 400, 10, 50);
   }
 }
 window.addEventListener("load", draw);
