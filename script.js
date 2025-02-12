5a56a98a62da5bd58db27982676fb848be6517a4
Jagger Gourley <jaggergourley@gmail.com>
2025-02-12 16:37:18 -0600

    Back at it yo

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+ctx.fillStyle = "white";
+
+let pongX = 400;
+let pongY = 400;
+let pongDX = 5;
+
 function draw() {
-  const canvas = document.getElementById("myCanvas");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "white";
-    ctx.fillRect(400, 400, 10, 10);
-    ctx.fillRect(10, 400, 10, 50);
-    ctx.fillRect(780, 400, 10, 50);
+  //console.log("draw");
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  ctx.fillRect(pongX, pongY, 10, 10);
+  function update() {
+    //console.log("update");
+    if (pongX + 3 == window.innerWidth + 5) {
+      pongDX *= -1;
+    }
+    pongX += pongDX;
+    ctx.fillRect(pongX, pongY, 10, 10);
   }
+  ctx.fillRect(10, 400, 10, 50);
+  ctx.fillRect(780, 400, 10, 50);
+  update();
+  window.requestAnimationFrame(draw);
 }
-window.addEventListener("load", draw);
+
+window.requestAnimationFrame(draw);
+
+//arrow up key code -> 38
+// down -> 40
+// w -> 87
+// s -> 83
