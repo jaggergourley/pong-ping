a2abadf8261812b05131c1e42eacdab78612b1ed
Jagger Gourley <jaggergourley@gmail.com>
2025-02-22 09:09:38 -0600

    scaling all the stuff

--- script.js
+++ script.js
@@ -13,29 +13,51 @@ ctx.fillStyle = "white";
 ctx.font = "50px 'PongScore', san-serif";
 ctx.textAlign = "center";
 
+const virtualWidth = 800;
+const virtualHeight = 600;
+
+function resizeCanvas() {
+  const screenHeight = window.innerHeight;
+  const screenWidth = window.innerWidth;
+
+  let scale = Math.min(
+    screenHeight / virtualHeight,
+    screenWidth / virtualWidth
+  );
+  scale *= 0.95;
+  console.log("scale:", scale);
+
+  canvas.style.height = `${canvas.height * scale}px`;
+  canvas.style.width = `${canvas.width * scale}px`;
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
 let pong = {
-  height: 10,
-  width: 10,
-  x: HEIGHT / 2 - 5,
-  y: WIDTH / 2 - 5,
+  height: virtualWidth * 0.01,
+  width: virtualWidth * 0.01,
+  x: virtualHeight / 2 - (virtualWidth * 0.01) / 2,
+  y: virtualWidth / 2 - (virtualWidth * 0.01) / 2,
   dx: 5,
-  dy: Math.random() * 10 - 5,
+  dy: Math.random() * (virtualWidth * 0.01) - (virtualWidth * 0.01) / 2,
 };
 
 let paddleLeft = {
-  height: 50,
-  width: 10,
-  x: 10,
-  y: HEIGHT / 2 - 25,
+  height: virtualHeight * 0.07,
+  width: virtualWidth * 0.01,
+  x: virtualWidth * 0.03,
+  y: virtualHeight / 2 - (virtualHeight * 0.07) / 2,
   dy: 10,
   score: 0,
 };
 
 let paddleRight = {
-  height: 50,
-  width: 10,
-  x: 780,
-  y: HEIGHT / 2 - 25,
+  height: virtualHeight * 0.07,
+  width: virtualWidth * 0.01,
+  x: virtualWidth * 0.97 - virtualWidth * 0.01,
+  y: virtualHeight / 2 - (virtualHeight * 0.07) / 2,
   dy: 10,
   score: 0,
 };
@@ -47,24 +69,24 @@ function update() {
   }
 
   if (keys["s"] && paddleLeft.y > 0) paddleLeft.y -= paddleLeft.dy;
-  if (keys["x"] && paddleLeft.y + paddleLeft.height < HEIGHT)
+  if (keys["x"] && paddleLeft.y + paddleLeft.height < virtualHeight)
     paddleLeft.y += paddleLeft.dy;
   if (keys["ArrowUp"] && paddleRight.y > 0) paddleRight.y -= paddleRight.dy;
-  if (keys["ArrowDown"] && paddleRight.y + paddleRight.height < HEIGHT)
+  if (keys["ArrowDown"] && paddleRight.y + paddleRight.height < virtualHeight)
     paddleRight.y += paddleRight.dy;
 }
 
 function collision() {
   // Top/bottom
-  if (pong.y <= 0 || pong.y + pong.height >= HEIGHT) {
+  if (pong.y <= 0 || pong.y + pong.height >= virtualHeight) {
     pong.dy *= -1;
   }
 
   // Right paddle
   if (
-    pong.x + pong.width == WIDTH - paddleRight.width * 2 &&
-    paddleRight.y <= pong.y - 5 &&
-    pong.y <= paddleRight.y + paddleRight.height + 5
+    pong.x + pong.width == virtualWidth - paddleRight.x &&
+    paddleRight.y <= pong.y - pong.height / 2 &&
+    pong.y <= paddleRight.y + paddleRight.height + pong.height / 2
   ) {
     pong.dx *= -1;
     pong.dy =
@@ -96,21 +118,21 @@ function pauseGame() {
 
 function resetPong() {
   if (pong.x + pong.width / 2 <= 0) {
-    pong.x = WIDTH / 2 - pong.width / 2;
-    pong.y = HEIGHT / 2 - pong.height / 2;
+    pong.x = virtualWidth / 2 - pong.width / 2;
+    pong.y = virtualHeight / 2 - pong.height / 2;
     paddleRight.score += 1;
     pauseGame();
   }
-  if (pong.x + pong.width / 2 > WIDTH) {
-    pong.x = WIDTH / 2 - pong.width / 2;
-    pong.y = HEIGHT / 2 - pong.height / 2;
+  if (pong.x + pong.width / 2 > virtualWidth) {
+    pong.x = virtualWidth / 2 - pong.width / 2;
+    pong.y = virtualHeight / 2 - pong.height / 2;
     paddleLeft.score += 1;
     pauseGame();
   }
 }
 
 function draw() {
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  ctx.clearRect(0, 0, virtualWidth, virtualHeight);
   if (!isPaused) {
     ctx.fillRect(pong.x, pong.y, pong.width, pong.height);
   }
@@ -121,8 +143,8 @@ function draw() {
     paddleRight.width,
     paddleRight.height
   );
-  ctx.fillText(paddleLeft.score, WIDTH / 4, 100);
-  ctx.fillText(paddleRight.score, (3 * WIDTH) / 4, 100);
+  ctx.fillText(paddleLeft.score, virtualWidth / 4, 100);
+  ctx.fillText(paddleRight.score, (3 * virtualWidth) / 4, 100);
 }
 
 function gameLoop() {
